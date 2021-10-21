4dd6e04d10aac58e17f673aa52c67b56428d3f7e
smokieeop <89496944+smokieeop@users.noreply.github.com>
2021-10-21 08:50:52 +0545

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-exports.TOKEN ="ODgyNTgyNDUyNjYzNjQwMTA0.YS9e9g.vL-fQTU7lz9DKgSqN3bCmAQAV7g";
-
-exports.PREFIX = '^';
-
-exports.news_API = 'a577c2d2d93544808ea1d8126941d63c';
-
-exports.giphy_API = '8bGdkZu55D2r5kffS5H9msNC56IX8CJ1';
-
-exports.AME_API = 'bb03f373caa534fcfcbaeae177a65134f44a6e57ba7a7b098be273867b376d8a677ddae3c23c6ded4fec8288573945e8c3483689deb13f229376ad4b5b60231d';
